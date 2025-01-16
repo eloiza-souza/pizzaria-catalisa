355ab22baa16434847d8e6e24b2ea146233aa946
Eloiza <eloiza.souza@zup.com.br>
2025-01-16 10:27:04 -0300

    feat: add class PagamentoDTO

---
+++ src/main/java/com/zup/pizzaria/dtos/PagamentoDTO.java
@@ -0,0 +1,39 @@
+package com.zup.pizzaria.dtos;
+
+import java.math.BigDecimal;
+
+public class PagamentoDTO {
+    private Long pedidoID;
+    private String formaPagamento;
+    private BigDecimal valorPago;
+
+    public PagamentoDTO(Long pedidoID, String formaPagamento, BigDecimal valorPago) {
+        this.pedidoID = pedidoID;
+        this.formaPagamento = formaPagamento;
+        this.valorPago = valorPago;
+    }
+
+    public Long getPedidoID() {
+        return pedidoID;
+    }
+
+    public void setPedidoID(Long pedidoID) {
+        this.pedidoID = pedidoID;
+    }
+
+    public String getFormaPagamento() {
+        return formaPagamento;
+    }
+
+    public void setFormaPagamento(String formaPagamento) {
+        this.formaPagamento = formaPagamento;
+    }
+
+    public BigDecimal getValorPago() {
+        return valorPago;
+    }
+
+    public void setValorPago(BigDecimal valorPago) {
+        this.valorPago = valorPago;
+    }
+}
