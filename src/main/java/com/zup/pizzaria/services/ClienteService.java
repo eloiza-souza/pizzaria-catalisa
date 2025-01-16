4374e0b06ea835d339ef2fcb1e2a79f57f44cb44
Eloiza <eloiza.souza@zup.com.br>
2025-01-16 11:37:00 -0300

    feat: add class ClienteRepository

---
+++ src/main/java/com/zup/pizzaria/services/ClienteService.java
@@ -0,0 +1,19 @@
+package com.zup.pizzaria.services;
+
+import com.zup.pizzaria.dtos.ClienteDTO;
+import com.zup.pizzaria.models.Cliente;
+import com.zup.pizzaria.repository.ClienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ClienteService {
+
+    @Autowired
+    private ClienteRepository clienteRepository;
+
+    public ClienteDTO criarCliente(Cliente cliente){
+        clienteRepository.save(cliente);
+        return new ClienteDTO(cliente.getId(), cliente.getNome(), cliente.getEmail());
+    }
+}
