f71b58e953a1b3a57bfb9381bca790cf1c0a0e43
Eloiza <eloiza.souza@zup.com.br>
2025-01-16 10:14:07 -0300

    feat: add class Pagamento

---
+++ src/main/java/com/zup/pizzaria/models/Pagamento.java
@@ -0,0 +1,70 @@
+package com.zup.pizzaria.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+import java.time.LocalDateTime;
+
+@Entity
+public class Pagamento {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long pedidoId;
+    private String formaPagamento;
+    private BigDecimal valorPago;
+    private Instant dataHoraPagamento;
+    public Pagamento() {
+    }
+
+    public Pagamento(Long pedidoId, String formaPagamento, BigDecimal valorPago) {
+        this.pedidoId = pedidoId;
+        this.formaPagamento = formaPagamento;
+        this.valorPago = valorPago;
+        this.dataHoraPagamento = Instant.now();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Long getPedidoId() {
+        return pedidoId;
+    }
+
+    public void setPedidoId(Long pedidoId) {
+        this.pedidoId = pedidoId;
+    }
+
+    public String getFormaPagamento() {
+        return formaPagamento;
+    }
+
+    public void setFormaPagamento(String formaPagamento) {
+        this.formaPagamento = formaPagamento;
+    }
+
+    public BigDecimal getValorPago() {
+        return valorPago;
+    }
+
+    public void setValorPago(BigDecimal valorPago) {
+        this.valorPago = valorPago;
+    }
+
+    public Instant getDataHoraPagamento() {
+        return dataHoraPagamento;
+    }
+
+    // Método para validar o pagamento
+    public void validarPagamento(BigDecimal valorTotalPedido) {
+        if (valorPago.compareTo(valorTotalPedido) < 0) {
+            throw new IllegalArgumentException("O valor pago não pode ser menor que o valor total do pedido.");
+        }
+    }
+}
