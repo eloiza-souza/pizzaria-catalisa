50481edff9ff7429c7274045a3d1c0d382abe2e9
Eloiza <eloiza.souza@zup.com.br>
2025-01-16 11:12:09 -0300

    feat: add validation to PagamentoService - criarPagamento"

--- src/main/java/com/zup/pizzaria/services/PagamentoService.java
+++ src/main/java/com/zup/pizzaria/services/PagamentoService.java
@@ -2,7 +2,9 @@ package com.zup.pizzaria.services;
 
 import com.zup.pizzaria.dtos.PagamentoDTO;
 import com.zup.pizzaria.models.Pagamento;
+import com.zup.pizzaria.models.Pedido;
 import com.zup.pizzaria.repository.PagamentoRepository;
+import com.zup.pizzaria.repository.PedidoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,7 +13,16 @@ public class PagamentoService {
     @Autowired
     private PagamentoRepository pagamentoRepository;
 
-    public PagamentoDTO criarPagamento(Pagamento pagamento){
+    @Autowired
+    private PedidoRepository pedidoRepository;
+
+    public PagamentoDTO criarPagamento(Pagamento pagamento) {
+        // Obtém o pedido
+        Pedido pedido = pedidoRepository
+                .findById(pagamento.getPedidoId())
+                .orElseThrow(() -> new RuntimeException("Pedido não encontrado"));
+
+        pagamento.validarPagamento(pedido.getValorTotal());
         pagamentoRepository.save(pagamento);
         return new PagamentoDTO(pagamento.getPedidoId(), pagamento.getFormaPagamento(), pagamento.getValorPago());
     }
